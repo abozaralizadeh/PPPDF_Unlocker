5db157c8eb23ef85ca40342c9528701ccb524794
GitHub Action <action@github.com>
2025-09-16 16:50:44 +0000

    Update upppdf formula to 1.0.5

--- homebrew-tap/Formula/upppdf.rb
+++ homebrew-tap/Formula/upppdf.rb
@@ -1,8 +1,8 @@
 class Upppdf < Formula
   desc "Unlock password protected PDF files using multiple methods"
   homepage "https://github.com/abozaralizadeh/UPPPDF"
-  url "https://github.com/abozaralizadeh/UPPPDF/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://github.com/abozaralizadeh/UPPPDF/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "ea2a33cda49c0abf595d72350a16b5f8a28ad36df6364caeac6831392e3bd08f"
   license "MIT"
 
   head "https://github.com/abozaralizadeh/UPPPDF.git", branch: "master"
